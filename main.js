405d3970902b94a62bb78943e1bb102155498c68
lumpenop <swk5373@gmail.com>
2021-03-25 17:12:42 +0900

    배너 자바스크립트 추가

---
+++ main.js
@@ -0,0 +1,36 @@
+visual = document.querySelectorAll('#visual > ul > li > img');
+index = 0;
+visualNav = document.querySelectorAll('.visual_nav > ul > li');
+
+
+for (i=0; i<visualNav.length; i++){
+    if(i==0){
+        visualNav[i].setAttribute("onclick","bannerBtn"+"("+i+")");
+        visualNav[i].setAttribute("id","selected");
+    }else{
+        visualNav[i].setAttribute("onclick","bannerBtn"+"("+i+")");
+    }
+}
+
+banner = (n) => {
+    if (n != undefined){
+        index = n;
+    }
+    document.querySelector(".on").removeAttribute("class")
+    document.querySelector("#selected").removeAttribute("id")
+    if (index == visual.length){
+        index = 0;
+    }
+    visual[index].setAttribute("class", "on")
+    visualNav[index].setAttribute("id", "selected")
+    index++;
+}
+rolling = setInterval(banner, 4500);
+
+
+
+bannerBtn = (n) => {
+    clearInterval(rolling);
+    banner(n)
+    rolling = setInterval(banner, 4500);
+}
