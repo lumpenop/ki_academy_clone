954c6c1bdd1ef915f856b38b54ea71c0bc5203cc
lumpenop <swk5373@gmail.com>
2021-03-31 11:07:55 +0900

    interview, top banner 기능 추가

--- main.js
+++ main.js
@@ -46,3 +46,74 @@ function out(){
     snb = document.querySelector(".snb_area")
     snb.className = "snb_area navi2";
 }
+
+
+function slider(){
+
+    li = document.querySelectorAll('.interview li');
+  
+    document.querySelector('.interview ul').className = 'move';
+
+    setTimeout(function(){
+        document.querySelector('.interview ul').appendChild(li[0]);
+        document.querySelector('.interview ul').className = 'move2';
+        }, 1000);
+     
+}
+
+rollin = setInterval(slider, 2000);
+
+function left(){
+    clearInterval(rollin);
+    slider()
+    rollin = setInterval(slider, 2000);
+}
+
+function right(){
+    clearInterval(rollin);
+    li = document.querySelectorAll('.interview li');
+    newLi=document.createElement('li');
+    newLi.innerHTML = li[3].innerHTML;
+
+    document.querySelector('.interview ul').prepend(newLi);
+    document.querySelector('.interview ul').removeAttribute('class');
+    document.querySelector('.interview ul').className = 'move_right';
+
+
+    setTimeout(function(){
+        document.querySelector('.interview ul').className = 'move_right2';
+        document.querySelector('.interview ul').removeChild(li[3]);
+        }, 100);
+    rollin = setInterval(slider, 2000);
+}
+
+li = document.querySelectorAll('.interview li');
+
+for (i=0; i< li.length; i++){
+    li[i].setAttribute("onmouseover","stop()");
+    li[i].setAttribute("onmouseout","restart()");
+
+}
+stop = () => {
+    clearInterval(rollin);
+
+}
+
+restart = () => {
+    clearInterval(rollin);
+    rollin = setInterval(slider, 2000);
+
+}
+
+rollRoll = () => {
+
+    document.querySelector('.top_banner_roll > ul').className = "rollroll";
+    li2 = document.querySelectorAll('.rollroll > li');
+    
+    setTimeout(function(){
+        document.querySelector('.top_banner_roll > ul').appendChild(li2[0])
+        document.querySelector('.top_banner_roll > ul').removeAttribute('class')
+    }, 1000);
+}
+
+setInterval(rollRoll, 2000);
